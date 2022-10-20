fc1e13ccff90b1ac721510d326e826aae96c6e8a
Ajay Alfredo Almani <49350537+ajayalf@users.noreply.github.com>
2022-10-21 01:21:42 +0700

    Create la2.c

---
+++ pert-2/la2.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+
+#include <stdlib.h>
+#include <conio.h>
+
+struct node{
+    int data;
+    struct node *rantai;
+};
+
+int main()
+{
+    struct node *depan = malloc(sizeof(struct node));
+    depan->data = 504;
+    depan->rantai = NULL;
+
+    struct node *tengah = malloc(sizeof(struct node));
+    tengah->data = 200;
+    tengah->rantai = NULL;
+    depan->rantai = tengah;
+
+    tengah = malloc(sizeof(struct node));
+    tengah->data = 93;
+    tengah->rantai = NULL;
+
+    depan->rantai->rantai = tengah;
+    int c;
+    printf("\n========================================\n"
+            "              PROGRAM ARRAY \n"
+            "========================================\n\n");
+    printf("\n1. Hitung Simpul");
+    printf("\n2. Cetak Data ");
+    printf("\n========================================\n");
+    printf("\nMasukkan Pilihan : ");
+    scanf("%d",&c);
+    if(c == 1){
+        int hitung = 0;
+        if(depan == NULL)
+        printf("Linked list kosong");
+        struct node *tunjuk = NULL;
+        tunjuk = depan;
+        while(tunjuk != NULL){
+            hitung++;
+            tunjuk = tunjuk->rantai;
+        }
+        printf("Jumlah Node pada Linked list adlaah %d", hitung);
+    }else if(c == 2){
+        if(depan == NULL)
+        printf("Linked list kosong");
+        struct node *tunjuk = NULL;
+        tunjuk = depan;
+        while(tunjuk != NULL){
+            printf("\nData pada linked list adalah %d", tunjuk->data);
+            tunjuk = tunjuk->rantai;
+        }
+    }else{
+        printf("Wrong Input\n");
+    }
+    return main();
+        
+}
